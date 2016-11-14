034d5519c5aab9360d19650a1bc464f476a05535
Zeuxis Lo <seekstudio@gmail.com>
2016-11-14 16:22:07 +0800

    clear the dummy index page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule Ubxui.PageController do
-  use Ubxui.Web, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
+    use Ubxui.Web, :controller
+
+    def index(conn, _params) do
+        render conn, "index.html"
+    end
+
 end
