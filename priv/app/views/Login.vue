6be039485872b54d3ab4eb59b3d8bb239ea7b062
Zeuxis Lo <seekstudio@gmail.com>
2016-11-17 18:05:03 +0800

    add ui for login and register page

--- priv/app/views/Login.vue
+++ priv/app/views/Login.vue
@@ -1,7 +1,32 @@
 <template>
     <div id="login">
         <div class="container">
-            Login
+            <h3>Login</h3>
+            <hr>
+            <div class="panel panel-default">
+                <div class="panel-heading">Account</div>
+                <div class="panel-body">
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <label for="username" class="col-sm-2 control-label">Username</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="username" name="username" placeholder="Username">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="col-sm-2 control-label">Password</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-default">Sign in</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- priv/app/views/Register.vue
+++ priv/app/views/Register.vue
@@ -1,7 +1,38 @@
 <template>
     <div id="register">
         <div class="container">
-            Register
+            <h3>Register</h3>
+            <hr>
+            <div class="panel panel-default">
+                <div class="panel-heading">Account</div>
+                <div class="panel-body">
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <label for="username" class="col-sm-2 control-label">Username</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="username" name="username" placeholder="Username">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="col-sm-2 control-label">Password</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="confirm-password" class="col-sm-2 control-label">Confirm Password</label>
+                            <div class="col-sm-10">
+                                <input type="confirm-password" class="form-control" id="confirm-password" name="confirm_password" placeholder="Confirm Password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-default">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </template>
