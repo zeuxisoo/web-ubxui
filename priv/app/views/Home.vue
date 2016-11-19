3547f20e06ccca0b8dae8a0f28f1571223f04851
Zeuxis Lo <seekstudio@gmail.com>
2016-11-19 11:18:06 +0800

    update home layout

--- priv/app/views/Home.vue
+++ priv/app/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
     <div id="home">
         <div class="container">
-            Home
+            <div class="jumbotron">
+                <h1>Ubxui</h1>
+                <p>A web interface for registered user to search related events without open target page</p>
+                <p>
+                    <router-link class="btn btn-success btn-lg" v-bind:to="{ name: 'login' }">
+                        Login
+                    </router-link>
+                    <router-link class="btn btn-primary btn-lg" v-bind:to="{ name: 'register' }">
+                        Register
+                    </router-link>
+                </p>
+            </div>
         </div>
     </div>
 </template>
