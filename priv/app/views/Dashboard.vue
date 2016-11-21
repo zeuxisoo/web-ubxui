b60280f068e3d83f1e7f0e306481c5df5bcf4d7c
Zeuxis Lo <seekstudio@gmail.com>
2016-11-21 13:34:09 +0800

    update dashboard ui

--- priv/app/views/Dashboard.vue
+++ priv/app/views/Dashboard.vue
@@ -1,7 +1,40 @@
 <template>
     <div id="dashboard">
         <div class="container">
-            Dashboard
+            <h3>Dashboard</h3>
+            <hr>
+            <div class="form-horizontal">
+                <div class="form-group">
+                    <label for="event-id" class="col-sm-2 control-label">Event Id</label>
+                    <div class="col-sm-8">
+                        <input type="email" class="form-control" id="event-id" placeholder="54321">
+                    </div>
+                    <div class="col-sm-2">
+                        <button type="submit" class="btn btn-default">Search</button>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>State</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </template>
